3ab96328b0ab2bd3e0074b0b85220f99fe6a8b9f
yangyafei <342363079@qq.com>
2017-09-05 15:22:06 +0800

    html-webpack-plugin和entry：不同html插入对应不同的js

--- mc_mall/src/index.js
+++ mc_mall/src/index.js
@@ -1,6 +1,1 @@
-var sub = require('./sub');
-
-var app = document.createElement('div');
-app.innerHTML = '<h1>Hello World</h1>';
-app.appendChild(sub());
-document.body.appendChild(app);
+console.log('hell');
--- mc_mall/webpack.config.js
+++ mc_mall/webpack.config.js
@@ -9,21 +9,29 @@ var ROOT_PATH = path.resolve(__dirname);
 var SRC_PATH = path.resolve(ROOT_PATH, 'src');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-var getHtmlWebpackPlugins = (function () {
+var getHtmlWebpackPlugins = function () {
     return pageInfo.map(function (page) {
         return new HtmlWebpackPlugin({
             title: page.title,
             filename: page.name + '.html',
+            chunks: [page.name],
             template: './html/template/'+ (page.template || 'common.ejs')
         })
     })
-})();
+};
+var getEntries = function () {
+    var entries = {};
+    pageInfo.forEach(function (page) {
+        entries[page.name] = './src/' + page.name + '.js';
+    });
+    return entries;
+}
 
 module.exports = {
-    entry: SRC_PATH,
+    entry: getEntries(),
     output: {
         path: BUILD_PATH,
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
-    plugins: getHtmlWebpackPlugins
+    plugins: getHtmlWebpackPlugins()
 }
